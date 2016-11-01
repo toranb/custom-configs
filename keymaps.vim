24221c5eba9d325c462871408a190b80f9b47a99
toran billups <toranb@gmail.com>
2016-11-01 16:20:25 -0500

    tweaks to fix clipboard/ag ignore

--- keymaps.vim
+++ keymaps.vim
@@ -4,3 +4,7 @@ nnoremap <Leader>nt :VimFilerExplorer -simple -parent -find<CR>
 nnoremap <Leader>a :Search<CR>
 nnoremap <Leader>fb :Unite buffer<CR>
 nnoremap <Esc><Esc> :nohlsearch<CR>
+map <Leader>js :%!python -m json.tool<CR>
+inoremap jk <ESC>
+set clipboard=unnamed
+au Filetype javascript setl et ts=4 sw=4
--- zsh.sh
+++ zsh.sh
@@ -29,6 +29,7 @@ alias push='git push origin master'
 alias pull='git pull --rebase origin master'
 alias ts='tig status'
 alias c='clear'
+alias ag='ag --ignore-dir=node_modules --ignore-dir=bower_components --ignore-dir=tmp --ignore-dir=dist --ignore-dir=vendor --ignore-dir=.git'
 
 export NVM_DIR="/Users/toranb/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
