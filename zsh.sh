2723838b7c155e960b49db71e68aa6003c3acabc
toran billups <toranb@gmail.com>
2016-11-29 18:06:49 -0600

    added include for rvm (zsh)

--- zsh.sh
+++ zsh.sh
@@ -35,3 +35,5 @@ alias stopdb='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
 export NVM_DIR="/Users/toranb/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
